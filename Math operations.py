488348cfe1b2b38f77d9055872d8470263c83600
Bernard Ian Santella <santellabernardian@gmail.com>
2024-10-16 14:41:51 +0400

    Import Turtle Exercise

--- Math operations.py
+++ Math operations.py
@@ -53,3 +53,31 @@ print(f'The value is {10 + 2}.')
 
 val = 10
 print(f'The value is {val + 2}.')
+
+num = 1000000.00
+print(f'{num:,.2f}')
+
+discount = 0.5
+print(f'{discount:.0%}')
+
+num = 123456789
+print(f'{num:,d}')
+
+num = 12345.6789
+print(f'{num:.2e}')
+
+num = 12345.6789
+print(f'The number is {num:12,.2f}')
+
+print(f'{num:<20.2f}') # right alignment
+print(f'{num:>20.2f}') # left alignment
+print(f'{num:^20.2f}') # center alignment
+
+amount = 0.069
+print(f'{amount}')
+
+INTEREST_RATE = 0.014
+print(f'{INTEREST_RATE}')
+
+import turtle
+turtle.forward(100)
